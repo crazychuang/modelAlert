20abcb689de61b16944dac6c86eefc714e92cfb6
boai <boai@boaimatoiMac.local>
2013-09-28 11:29:50 +0800

    Initial Commit

---
+++ testmodelalert/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testmodelalert
+//
+//  Created by boai on 13-9-28.
+//  Copyright (c) 2013年 bravetorun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
